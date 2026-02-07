37c082dd0f64db16452ac5c179f9493516f17fcf
Nikhil Yadav <nikhilyadav200530@gmail.com>
2026-02-07 11:25:01 +0530

    fix: change agora-access-token import to CommonJS compatible syntax

--- server/index.js
+++ server/index.js
@@ -2,7 +2,8 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import http from 'http';
-import { RtcTokenBuilder, RtcRole } from 'agora-access-token';
+import pkg from 'agora-access-token';
+const { RtcTokenBuilder, RtcRole } = pkg;
 
 dotenv.config();
 
