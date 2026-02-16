a138e158f69e7afe801036365e845a398591c887
Nikhil Yadav <nikhilyadav200530@gmail.com>
2026-02-16 19:35:20 +0530

    Clean up Developers.tsx by removing imports and footer
    Removed unused imports and footer information.

--- client/src/pages/Developers.tsx
+++ client/src/pages/Developers.tsx
@@ -1,13 +1,3 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
 
-
-        
-
-        <div className="mt-16 text-center">
-          <p className="text-gray-600 text-xs font-mono">
-            Running v3.0.0-turbo • Made with ❤️ in India
-          </p>
-        </div>
 
       
